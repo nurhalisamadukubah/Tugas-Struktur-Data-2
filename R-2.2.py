aedd4964b5a02c1bdb013a86c19ee4f702de7bc3
nurhalisamadukubah <58254317+nurhalisamadukubah@users.noreply.github.com>
2021-06-17 13:44:05 +0700

    Add files via upload

---
+++ R-2.2.py
@@ -0,0 +1,4 @@
+#Berikan contoh aplikasi perangkat lunak di mana kemampuan beradaptasi dapat berarti perbedaan antara umur penjualan yang lama dan kebangkrutan.
+
+#memiliki kemampuan beradaptasi berarti kode tersebut bersifat fleksibel sehingga dapat dijalankan di berbagai jenis perangkat keras dan di sistem operasi yang berbeda.
+#jadi suatu perangkat lunak harus mampu merespon dengan cepat ketika lingkungan berubah.
---
+++ R-2.3.py
@@ -0,0 +1,5 @@
+#Jelaskan komponen dari GUI editor teks dan metode yang dienkapsulasinya.
+
+#Salah satu komponennya adalah pengenalan bahasa sehingga mewarnai kode dengan tepat.
+#Metodenya termasuk memeriksa ekstensi file.
+#Jika ekstensi file tidak dimasukkan, itu akan gunakan skema kode warna default pada teks.
