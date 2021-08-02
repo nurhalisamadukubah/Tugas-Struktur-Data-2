8a9f100f645054ca820da2558912eb80312e30ea
nurhalisamadukubah <58254317+nurhalisamadukubah@users.noreply.github.com>
2021-08-02 14:18:47 +0700

    Update R-2.1.py

--- R-2.1.py
+++ R-2.1.py
@@ -3,3 +3,4 @@
 # Robotic Surgery machines(robot untuk mengoperasi pasien di rumah sakit)
 # insulin checker (pengecek kadar insulin pada darah bagi penderita diabetes)
 # flight controllers (pengendali pesawat)
+3
